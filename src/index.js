9ad18ec35916674091a0543ff2a08e17c41ed243
vladdddcher <vladdddcher@gmail.com>
2023-01-07 12:16:04 +0300

    only html, css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.html';
+// import './style.scss';
+import './scss/style.scss';
+
+import { mult, sum } from './modules/calc';
+
+
+
+console.log(mult(3, 4));
+console.log(sum(3, 4));
+console.log('hi')
